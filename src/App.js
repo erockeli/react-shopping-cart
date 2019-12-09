cac0080581e768b3b480eeb58eaa084f87effec6
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-12-09 16:07:09 -0600

    Bring in state via useContext

--- src/App.js
+++ src/App.js
@@ -19,16 +19,13 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{cart,removeItem}}>
+				<CartContext.Provider value={{cart}}>
 					<Navigation cart={cart} />
 					<Route exact path="/" component={Products} />
 					<Route path="/cart" component={ShoppingCart} />
 				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
-
-		
-
 	);
 }
 
